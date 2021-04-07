94e24ab6171f5673d733883a81e336e032ecf1f1
thaihuutai <81974701+thaihuutai@users.noreply.github.com>
2021-04-07 19:14:33 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://www.blogger.com/blog/posts/6765060838573214536'>http://faceseo.vn"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://thaihuutaicanho577.blogspot.com/'>https://thaihuutaicanho577.blogspot.com/</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
